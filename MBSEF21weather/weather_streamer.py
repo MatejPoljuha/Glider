9ce19dfddfe160ef424a895655d08997cc2479ca
FelixEL <30323886+FelixEL@users.noreply.github.com>
2021-11-23 20:11:33 +0100

    added weather streamer

--- MBSEF21weather/weather_streamer.py
+++ MBSEF21weather/weather_streamer.py
@@ -3,13 +3,36 @@ import time
 import json
 import socket
 from datetime import datetime
-from communication import *
+import glob
+from communication import receive_server, send_client
+import os
+from time import sleep
 
 
-with open("weather.json", "r") as read_file:
-    json_received = json.loads(read_file.read())
+jsonfiles = []
+for file in glob.glob("*.json"):
+    jsonfiles.append(file)
+    
+
+for fil_name in jsonfiles: 
+    fil_dir = os.path.dirname(os.path.abspath(__file__))
+
+    json_dict  =None
+    with open(fil_dir + '/' +fil_name, "r") as read_file:
+        json_dict = json.loads(read_file.read())
+        
+        
+    send_client(destination_port=1505, input_dict=json_dict)
+    sleep(5)
+    print(json_dict)
 
 
-print(json_received)
+
+#print(txtfiles)    
+    
+
+
+
+#print(json_received)
 
 #send_client(destination_port=1501, input_dict=dict_list)
