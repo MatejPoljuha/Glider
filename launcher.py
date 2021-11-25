ccbd167a3c1e3f92986383fd5e48b05bec53ca4b
FelixEL <30323886+FelixEL@users.noreply.github.com>
2021-11-25 19:07:28 +0100

    pushhhh

--- launcher.py
+++ launcher.py
@@ -1,11 +1,13 @@
 import subprocess
 
-from MBSEF21weather.weather_streamer import *
-from MBSEF21DPS.data_processing_system import *
-from MBSEF21GUI.GUI import *
-
-
-
+#from MBSEF21weather.weather_streamer import *
+#from MBSEF21DPS.data_processing_system import *
+#from MBSEF21GUI.GUI import *
+import os
+#os.system("start python MBSEF21GUI/GUI.py ") 
+subprocess.Popen(["python", "MBSEF21DPS/data_processing_system.py"] )
+subprocess.Popen(["python", "MBSEF21weather/weather_streamer.py"] )
+subprocess.Popen(["python", "MBSEF21GUI/GUI.py"] )
 #subprocess.Popen(["python", "MBSEF21GUI/GUI.py"])
 #print("der")
 #subprocess.call("data_processing_system.py")
